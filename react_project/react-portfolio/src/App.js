809e512a0b053faeec37b76043ea59002e11e61f
Eunbi Kim <kimeunb1822@gmail.com>
2020-08-04 10:45:59 -0400

    test

--- react_project/react-portfolio/src/App.js
+++ react_project/react-portfolio/src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    selectedFile: null,
+    name: "Eunbi",
+    jsonfd: {name: "Kim", bloodtype: "O"}
+  }
+  fileSelectedHandler = event =>
+  {
+    this.setState({
+      selectedFile: event.target.files[0]
+    })
+  }
+  fileIploadHandler = ()=>{
+    const fd = new FormData();
+    const product_id_list = JSON.stringify(['pid1234', 'pid1235']);
+    const obj = {
+      hello: "world"
+    };
+
+    const json = JSON.stringify(obj);
+    fd.append("arr", product_id_list);
+    fd.append("name", this.state.name);
+     fd.append("jsonfd", JSON.stringify(this.state.jsonfd));
+     //fd.append("blob", blob);
+    fd.append('profilepic', this.state.selectedFile, this.state.selectedFile.name);
+
+
+    
+    axios.post('http://localhost:3003/auth/mtregister', fd, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'multipart/form-data'
+      }
+      })
+    .then(res => {
+      console.log(res);
+    })
+
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <input type="file" onChange={this.fileSelectedHandler}/>
+        <button onClick={this.fileIploadHandler}>Upload</button>
+      </div>
+    );
+  }
 }
 
 export default App;
